fd0ac03bea90569ee13892246c26941114e1949b
Thew Dhanat <26767607+ThewBear@users.noreply.github.com>
2020-06-09 17:11:45 +0700

    Add EMAIL_ARG

--- init.sh
+++ init.sh
@@ -1,8 +1,16 @@
 #!bin/bash
+
+if [[ -z "${EMAIL}" ]]
+then
+  EMAIL_ARG="-m ${EMAIL}"
+else
+  EMAIL_ARG="--register-unsafely-without-email"
+fi
+
 certbot certonly -n \
   --webroot -w $WebRoot \
   --rsa-key-size $RSAKeySize \
-  --agree-tos -m $EMAIL \
+  --agree-tos EMAIL_ARG \
   -d $DOMAIN
 
 cron -f
