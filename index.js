f3a16b66fa3459467d57cdfc6ff4ad27fe0f14e8
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-26 18:17:19 +0700

    Fix racing certbot using promise

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const { exec, execSync } = require("child_process");
+const { exec } = require("child_process");
+const { promisify } = require("util");
 const fs = require("fs");
+
+const exec = promisify(exec);
 
 const config = JSON.parse(
   process.env.config || fs.readFileSync("certbot.json", { encoding: "utf-8" })
@@ -13,26 +16,25 @@ const RSAKeySize = config.rsakeysize || 4096;
 
 const certificate_domains = config.domains.map((domain) => domain.join(","));
 
-function callback(error, stdout, stderr) {
-  console.error(error);
-  console.log(stdout);
-  console.warn(stderr);
+function renew() {
+  exec("certbot renew").then((stdout, stderr) => {
+    console.log(stdout);
+    console.warn(stderr);
+  });
+}
 
-  if (stderr) {
-    return;
+async function generate_cert() {
+  for (certificate_domain of certificate_domains) {
+    await exec(
+      `certbot certonly --standalone -n --rsa-key-size ${RSAKeySize} --agree-tos ${email_arg} ${staging_arg} -d ${certificate_domain}`
+    ).then((stdout, stderr) => {
+      console.log(stdout);
+      console.warn(stderr);
+    });
   }
 }
 
-function renew() {
-  exec("certbot renew", callback);
-}
-
-certificate_domains.map((certificate_domain) => {
-  execSync(
-    `certbot certonly --standalone -n --rsa-key-size ${RSAKeySize} --agree-tos ${email_arg} ${staging_arg} -d ${certificate_domain}`,
-    callback(error, stdout, stderr)
-  );
-});
+generate_cert();
 
 renew();
 
